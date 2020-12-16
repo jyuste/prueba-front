c1ff788fb2392afd9c8b4e3d572232461ba8cd31
Jose Yuste <jose.yuste@gmail.com>
2020-12-16 18:13:36 +0100

    Getting data from form
    - Mapping the initial data into a small array
    - Call the data from the input form

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
+import GitHubGetData from '../data/GitHubData'
+
 const SearchBox = () => {
 
+    //Made this handler in a handleSubmit function in order to keep the calls to the API minimized.
+    //It could be in an onChange in the input field if we need realtime search.
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const queryString = event.target.value
+        GitHubGetData(queryString)
+    }
+
     return (
-    <form>
+    <form onSubmit={handleSubmit}>
         <div className="control has-icons-right">
             <div className="field is-horizontal">
                 <div className="field-label is-normal">
--- src/data/GitHubData.js
+++ src/data/GitHubData.js
@@ -2,17 +2,30 @@ import axios from 'axios';
 
 const API = 'https://api.github.com/search/repositories?q='
 
+//API parameters have been individualized to be able to make filters if necessary in the future
+//Passing them as parameters in the main call as follows:
+// GitHubGetData(searchStr, page, resultsPerPage, sortBy, order)
+
 const GitHubGetData = (searchStr) => {
+
         let queryString = '?q=' + encodeURIComponent(searchStr);
         let page = '&page=1'
         let perPage = '&per_page=10'
         let sort ='&sort=stars'
         let order = '&order=desc'
+
         axios.get(API+queryString+page+perPage+sort+order)
         .then(res => {
-        const data = res.data;
-        console.log(data);
-        return data
+        const data = res.data.items;
+        //lets make a more manageable array to work with.
+        const dataBasic = data.map(result => (
+            {
+                name: result.name,
+                url: result.url
+            }
+        ))
+        return dataBasic
+
     })
 }
 
